3f512a783f60ce0b30828a25d31e326d5082fa5c
Regina <laulessr@gmail.com>
2015-03-15 12:04:54 -0500

    Login form pre-bootstrap

---
+++ session/new.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<form action="/session/create.php" method="post">
+    <label>Email: <input name="email" type="email"></label>
+    </br>
+    <label>Password: <input name="password" type="password"/></label>
+</form>
+</body>
+</html>
