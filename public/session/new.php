b5b20c0d1a87e256052ff1a68fdb89ec970aa004
Regina <laulessr@gmail.com>
2015-03-15 22:42:30 -0500

    Remove unused remember me checkbox

--- public/session/new.php
+++ public/session/new.php
@@ -44,11 +44,6 @@
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus name="email">
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required name="password">
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-        </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
         <p id="form-errors">
 
